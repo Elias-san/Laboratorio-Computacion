6c5e0a30a1e8c35fa878f44e4486b1fb4c934478
Elias San Martin <elias.smartin@gmail.com>
2022-09-25 18:28:47 -0300

    ej 1-4

---
+++ tp4/Ejercicio_Nro_3/Ejercicio_Nro_3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main (){
+
+    int nro=20;
+
+    while (nro > 0){
+        cout << nro << endl;
+        nro--;
+    }
+
+    return 0;
+}
---
+++ tp4/Ejercicio_Nro_4/Ejercicio_Nro_4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main (){
+
+    int nro,i=1;
+
+    cout << "Ingrese un numero: " << endl;
+    cin >> nro;
+    while (i <= nro){
+        cout << i << endl;
+        i++;
+    }
+
+    return 0;
+}
